8b4ff9109fff64c014d5f542ec095dda6e4c4ff3
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2026-05-31 00:23:02 +0200

    include/isl/hmap*: extract out include/isl/hbase*
    The extracted core functionality will be reused
    by include/isl/hset* in the next commit.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- include/isl/hmap.h
+++ include/isl/hmap.h
@@ -1,6 +1,5 @@
 #include <isl/ctx.h>
 #include <isl/maybe.h>
-#include <isl/printer.h>
 
 #define ISL_HBASE			ISL_HMAP
 #define ISL_HBASE_SUFFIX		ISL_HMAP_SUFFIX
@@ -11,6 +10,13 @@
 #define ISL_HBASE_HAVE_READ_FROM_STR
 #endif
 
+#include <isl/hbase.h>
+
+#undef ISL_HBASE
+#undef ISL_HBASE_SUFFIX
+#undef ISL_HBASE_IS_EQUAL
+#undef ISL_HBASE_HAVE_READ_FROM_STR
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -19,16 +25,6 @@ extern "C" {
 #define ISL_CAT(A,B) ISL_xCAT(A,B)
 #define ISL_xFN(TYPE,NAME) TYPE ## _ ## NAME
 #define ISL_FN(TYPE,NAME) ISL_xFN(TYPE,NAME)
-
-struct __isl_export ISL_HBASE;
-typedef struct ISL_HBASE	ISL_HBASE;
-
-__isl_constructor
-__isl_give ISL_HBASE *ISL_FN(ISL_HBASE,alloc)(isl_ctx *ctx, int min_size);
-__isl_give ISL_HBASE *ISL_FN(ISL_HBASE,copy)(__isl_keep ISL_HBASE *hbase);
-__isl_null ISL_HBASE *ISL_FN(ISL_HBASE,free)(__isl_take ISL_HBASE *hbase);
-
-isl_ctx *ISL_FN(ISL_HBASE,get_ctx)(__isl_keep ISL_HBASE *hbase);
 
 __isl_give ISL_MAYBE(ISL_VAL) ISL_FN(ISL_HMAP,try_get)(
 	__isl_keep ISL_HMAP *hmap, __isl_keep ISL_KEY *key);
@@ -39,8 +35,6 @@ __isl_give ISL_VAL *ISL_FN(ISL_HMAP,get)(__isl_keep ISL_HMAP *hmap,
 __isl_export
 __isl_give ISL_HMAP *ISL_FN(ISL_HMAP,set)(__isl_take ISL_HMAP *hmap,
 	__isl_take ISL_KEY *key, __isl_take ISL_VAL *val);
-__isl_give ISL_HBASE *ISL_FN(ISL_HBASE,drop)(__isl_take ISL_HBASE *hbase,
-	__isl_take ISL_KEY *key);
 
 isl_stat ISL_FN(ISL_HMAP,foreach)(__isl_keep ISL_HMAP *hmap,
 	isl_stat (*fn)(__isl_take ISL_KEY *key, __isl_take ISL_VAL *val,
@@ -50,22 +44,6 @@ isl_bool ISL_FN(ISL_HMAP,every)(__isl_keep ISL_HMAP *hmap,
 	isl_bool (*test)(__isl_keep ISL_KEY *key, __isl_keep ISL_VAL *val,
 		void *user),
 	void *user);
-
-#ifdef ISL_HBASE_IS_EQUAL
-__isl_export
-isl_bool ISL_HBASE_IS_EQUAL(__isl_keep ISL_HBASE *hbase1,
-	__isl_keep ISL_HBASE *hbase2);
-#endif
-
-#ifdef ISL_HBASE_HAVE_READ_FROM_STR
-__isl_constructor
-__isl_give ISL_HBASE *ISL_FN(ISL_HBASE,read_from_str)(isl_ctx *ctx,
-	const char *str);
-#endif
-__isl_give char *ISL_FN(ISL_HBASE,to_str)(__isl_keep ISL_HBASE *hbase);
-__isl_give isl_printer *ISL_FN(isl_printer_print,ISL_HBASE_SUFFIX)(
-	__isl_take isl_printer *p, __isl_keep ISL_HBASE *hbase);
-void ISL_FN(ISL_HBASE,dump)(__isl_keep ISL_HBASE *hbase);
 
 #undef ISL_xCAT
 #undef ISL_CAT
@@ -80,8 +58,3 @@ void ISL_FN(ISL_HBASE,dump)(__isl_keep ISL_HBASE *hbase);
 #if defined(__cplusplus)
 }
 #endif
-
-#undef ISL_HBASE
-#undef ISL_HBASE_SUFFIX
-#undef ISL_HBASE_IS_EQUAL
-#undef ISL_HBASE_HAVE_READ_FROM_STR
